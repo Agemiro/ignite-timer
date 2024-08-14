0bb1e55881229c42e03d31b288392a30f324d53c
Agemiro Alves de Sousa Neto <agemiroalves11@gmail.com>
2024-08-14 21:43:38 +0200

    feat(home): create home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,47 @@
+import { Play } from "phosphor-react";
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">I'm going to work at</label>
+          <TaskInput id="task" placeholder="Give your project a name" />
+
+          <label htmlFor="minutesAmount">for</label>
+
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Start
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+}
